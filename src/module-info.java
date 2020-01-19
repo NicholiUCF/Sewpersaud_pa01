a60cc7f5911d4c12aea6a7680ff07ae2cc00f138
NicholiUCF <60024029+NicholiUCF@users.noreply.github.com>
2020-01-18 21:11:00 -0500

    my assignment

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,5 @@
-class A  {
-public static void main(String args[]) {
-		system.out.println("Hello World!");
-	}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This will be printed");
+    }
 }
